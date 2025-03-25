83bb373c17eb2ffb07360a704f70c652549f7288
Igor Santana <igor.j.santana@outlook.com.br>
2025-03-24 21:02:03 -0300

    corrigido menos de um dia

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
     const minutes = Math.floor((ms / 1000 / 60) % 60);
     const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
     const days = Math.floor(ms / (1000 * 60 * 60 * 24));
-    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    return `${hours}h ${minutes}m ${seconds}s`;
   };
 
   return (
